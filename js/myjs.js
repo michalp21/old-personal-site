14c42f855e0af8cc13951b89e983136bfdc92fee
mp3242 <mp3242@columbia.edu>
2017-12-30 10:19:11 -0500

    First version of my website.

---
+++ js/myjs.js
@@ -0,0 +1,9 @@
+$(document).ready(function(e) {
+	$('.panel').addClass('rounded');
+	$('.image').addClass('rounded');
+	$('.description').addClass('rounded');
+
+	// $('a').hover(function() {
+	//     $('body').animate({backgroundColor: '#FF0000'}, 'slow');
+	// });
+});

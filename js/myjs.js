bb8bfa688018a8471791b447b07e8c7643b32c67
mp3242 <mp3242@columbia.edu>
2017-12-30 23:38:39 -0500

    Fading scroll

--- js/myjs.js
+++ js/myjs.js
@@ -1,9 +1,13 @@
 $(document).ready(function(e) {
-	$('.panel').addClass('rounded');
-	$('.image').addClass('rounded');
-	$('.description').addClass('rounded');
+    $('.panel').addClass('rounded');
+    $('.image').addClass('rounded');
+    $('.description').addClass('rounded');
 
-	// $('a').hover(function() {
-	//     $('body').animate({backgroundColor: '#FF0000'}, 'slow');
-	// });
+    $(window).scroll(function(){
+        $(".scroll").css("opacity", (400-$(window).scrollTop()) /200);
+    });
+
+    // $('a').hover(function() {
+    //     $('body').animate({backgroundColor: '#FF0000'}, 'slow');
+    // });
 });
